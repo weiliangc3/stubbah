7aac8231b38908f58fd704e800019c15dc721b9f
Wei <weiliang.c3@gmail.com>
2021-03-05 00:22:33 +0000

    Add matched request timestamp

--- src/classes/MatchedRequest.ts
+++ src/classes/MatchedRequest.ts
@@ -9,10 +9,13 @@ export default class MatchedRequest {
 
   url: string;
 
+  time: Date;
+
   constructor(req: Request) {
     this.body = req.body;
     this.method = req.method;
     this.headers = req.headers;
     this.url = req.originalUrl;
+    this.time = new Date();
   }
 }
